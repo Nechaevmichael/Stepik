7dbe273a4009dd84435ffa446c0bf059e954edd6
NechaevMichael <nechaevmichael@mail.ru>
2022-10-10 21:21:41 +0500

    add Task053

---
+++ Task053.py
@@ -0,0 +1,12 @@
+# Программа получает на вход строку и ваша задача вывести первый элемент данной строки
+
+# Sample Input 1:
+
+# Zemfira
+# Sample Output 1:
+
+# Z
+
+text = input()
+result = text[0]
+print(result)
