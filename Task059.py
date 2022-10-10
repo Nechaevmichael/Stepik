bd0d76d036f2b651d06c7c1cd23d781b37b901f8
NechaevMichael <nechaevmichael@mail.ru>
2022-10-10 21:45:17 +0500

    add Task059

---
+++ Task059.py
@@ -0,0 +1,11 @@
+# Программа получает на вход строку. Ваша задача развернуть строку и вывести ее на экран.
+
+# Sample Input:
+
+# leetcode.com
+# Sample Output:
+
+# moc.edocteel
+
+text = input()
+print(text[::-1])
