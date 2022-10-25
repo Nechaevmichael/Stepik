dc72d3c4e10eb8ae10009bf51f744e1777ba5c07
NechaevMichael <nechaevmichael@mail.ru>
2022-10-25 21:10:01 +0500

    add Task162

--- Task162.py
+++ Task162.py
@@ -5,3 +5,13 @@ a, b = map(int, input().split())
 while b > 0:
     a, b = b, a % b
 print(a)
+
+# Найти НОД методом вычитания
+
+a, b = map(int, input().split())
+while a != b:
+    if a > b:
+        a = a - b
+    else:
+        b = b - a
+print(a)
