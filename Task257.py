f0800d6a6df02d4e407b2d996d04479040352d9b
NechaevMichael <nechaevmichael@mail.ru>
2022-11-20 12:38:57 +0500

    add Task257

---
+++ Task257.py
@@ -0,0 +1,8 @@
+# Создайте список первых букв каждого слова из строки st и выведите его на экран
+
+st = 'Create a list of the first letters of every word in this string'
+
+st = st.split()
+
+st = [i[0] for i in st]
+print(st)
