1ef3e23207d73efcc75c3e00602f3e033171e9cb
NechaevMichael <nechaevmichael@mail.ru>
2022-12-17 21:23:22 +0500

    add Task305

---
+++ Task305.py
@@ -0,0 +1,17 @@
+# Перед вами располагается множество  my_set 
+
+# Ваша задача добавить в него 4 строковых элемента: 
+
+# concert
+# brown
+# jacket
+# value
+
+# my_set = {'government', 'control', 'winter', 'few', 'generation', 'service', 'national', 'tradition', 'government'}
+my_set = set()
+my_set.add('concert')
+my_set.add('brown')
+my_set.add('jacket')
+my_set.add('value')
+# my_set.update('concert', 'brown', 'jacket', 'value')
+print(my_set)
