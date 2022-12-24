7225ba2569f7f665dd5b174cbe374aa06655a818
NechaevMichael <nechaevmichael@mail.ru>
2022-12-24 11:32:16 +0500

    add Task323

---
+++ Task323.py
@@ -0,0 +1,10 @@
+# Напишите функцию exponentiation, которая принимает на вход целое число и выводит на экран через пробел квадрат и куб этого числа. 
+
+# Вам необходимо написать только определение функции exponentiation
+
+def exponentiation(exp):
+    exp_1 = exp ** 2
+    exp_2 = exp ** 3
+    print(exp_1, exp_2)
+
+exponentiation(3)
