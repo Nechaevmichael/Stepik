b69ebaa0750a99f5732fc41fe843729ed03d6f55
NechaevMichael <nechaevmichael@mail.ru>
2023-01-02 13:30:00 +0500

    add Task353

---
+++ Task353.py
@@ -0,0 +1,22 @@
+# Напишите функцию only_one_positive, которая принимает произвольное количество числовых аргументов и возвращает True, 
+# когда из всех переданных значений только одно положительное, в противном случае верните False
+
+# Вам необходимо написать только определение функции only_one_positive
+
+# Ниже примеры вызова
+
+# only_one_positive(1, 2) -> False
+# only_one_positive(-1, 0, -3, 5, -3) -> True
+# only_one_positive() -> False
+
+def only_one_positive(*args):
+    count = 0
+    for i in args:
+        if i > 0:
+            count += 1
+    if count == 1:
+        return True
+    else:
+        return False
+
+print(only_one_positive(-1, 0, -3, 5, -3))
