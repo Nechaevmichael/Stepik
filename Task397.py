6d0fa82960819154831d255159f3aa6d26120e27
NechaevMichael <nechaevmichael@mail.ru>
2023-01-31 19:06:56 +0500

    add Task397

---
+++ Task397.py
@@ -0,0 +1,14 @@
+# Из модуля string импортируйте следующие переменные: 
+
+# ascii_lowercase - строка, содержащая английский буквы англ. алфавита в нижнем регистре
+# ascii_uppercase - строка, содержащая английский буквы англ. алфавита в верхнем регистре
+# punctuation - строка, содержащая символы пунктуации
+# Необходимо в отдельных строках вывести сперва все знаки пунктуации, затем заглавные символы и уже потом маленькие.
+
+from string import ascii_lowercase, ascii_uppercase, punctuation, ascii_letters
+print(punctuation)
+print(ascii_uppercase)
+print(ascii_lowercase)
+print(ascii_letters)
+
+
