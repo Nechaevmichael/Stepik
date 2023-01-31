e0b02a0f266b5caa96a13674a74cf9e44825512f
NechaevMichael <nechaevmichael@mail.ru>
2023-01-31 19:01:16 +0500

    add Task396

---
+++ Task396.py
@@ -0,0 +1,4 @@
+# Импортируйте стандартный модуль sys и при помощи функции getrecursionlimit узнайте лимит предела рекурсии. Его необходимо вывести на экран
+
+import sys
+print(sys.getrecursionlimit())
